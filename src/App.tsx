5c1d6155525a2730f9969b2be4fa0c88495a4553
Purushothaman Babu <purushot.29@gmail.com>
2025-11-11 18:26:21 +0530

    code update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/GoldChakra.png'
 import Murugan from './assets/BalamuruganWithPeacock.png'
 import Footer from "./components/Footer/Footer";
@@ -6,8 +5,6 @@ import Header from "./components/Header/Header";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
      <div className="app">
       <Header
